1cec83bc41b5732b94529fcb24bd0c639fa84797
KarinaBertosh <cherrycake246@gmail.com>
2023-05-31 19:06:57 +0300

    feat: rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
-const rename = async () => {
-    // Write your code here 
+const rename = () => {
+  const fs = require("fs");
+  const file = "./fs/files/wrongFilename.txt";
+  const newFile = "./fs/files/properFilename.md";
+
+  if (fs.existsSync(file) && !fs.existsSync(newFile)) {
+    fs.rename(file, newFile, () => {
+      console.log("File Renamed!");
+    });
+  } else {
+    return console.log(`Error: FS operation failed`);
+  }
 };
 
-await rename();
+rename();
