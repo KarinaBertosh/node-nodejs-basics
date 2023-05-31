5f8f3f6511f32f63830185092b1123fb727544e5
KarinaBertosh <cherrycake246@gmail.com>
2023-05-31 19:21:12 +0300

    feat: list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
-const list = async () => {
-    // Write your code here 
+const list = () => {
+  const fs = require("fs");
+  const files = "./fs/files";
+
+  if (fs.existsSync(files)) {
+    fs.readdir(files, (err, file) => {
+      console.log(file);
+    });
+  } else {
+    return console.log(`Error: FS operation failed`);
+  }
 };
 
-await list();
+list();
