283ed17c3703d980c93c9bcfd337c194f128dda2
KarinaBertosh <cherrycake246@gmail.com>
2023-05-31 17:38:35 +0300

    feat: create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
-const create = async () => {
-    // Write your code here 
+const fs = require("fs");
+var path = require('path');
+const create = () => {
+  const file = path.join(__dirname, "./files/fresh.txt");
+  if (!fs.existsSync(file)) {
+    fs.writeFile(file, "I am fresh and young", function (err) {
+      console.log("File created");
+    });
+  } else {
+    return console.log(`Error: FS operation failed`);
+  }
 };
 
-await create();
+create();
