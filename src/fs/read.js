5a5d676b3f8100ea75de3d4a35526a5e7a855044
KarinaBertosh <cherrycake246@gmail.com>
2023-05-31 19:25:55 +0300

    feat: read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
-const read = async () => {
-    // Write your code here 
+const read = () => {
+  const fs = require("fs");
+  const file = "./fs/files/fileToRead.txt";
+  if (fs.existsSync(file)) {
+    const file = fs.createReadStream("./fs/files/fileToRead.txt", "utf-8");
+    file.on("data", (chunk) => console.log(chunk));
+  } else {
+    return console.log(`Error: FS operation failed`);
+  }
 };
 
-await read();
+read();
