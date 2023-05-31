260cc83b37acd40396abec4a7ef68d5f57c98d73
KarinaBertosh <cherrycake246@gmail.com>
2023-05-31 19:15:39 +0300

    feat: delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
-const remove = async () => {
-    // Write your code here 
+const remove = () => {
+  const fs = require("fs");
+  const file = "./fs/files/fileToRemove.txt";
+
+  if (fs.existsSync(file)) {
+    fs.unlink(file, () => {
+      console.log("File delete");
+    });
+  } else {
+    return console.log(`Error: FS operation failed`);
+  }
 };
 
-await remove();
+remove();
