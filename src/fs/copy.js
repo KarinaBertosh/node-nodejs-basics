c790124ac65663104d305f9502db2854d743951f
KarinaBertosh <cherrycake246@gmail.com>
2023-05-31 18:56:52 +0300

    feat: copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
 const copy = async () => {
-    // Write your code here 
+  const fs = require("fs");
+  const files = "./fs/files";
+  const filesCopy = "./fs/files-copy";
+
+  if (fs.existsSync(files) && !fs.existsSync(filesCopy)) {
+    fs.mkdir(filesCopy, (err) => {});
+    fs.readdir(files, (err, file) => {
+      file.forEach((file) => {
+        fs.copyFile(`./fs/files/${file}`, `./fs/files-copy/${file}`, (err) => {
+          if (err) return err;
+        });
+      });
+      console.log("Files copied");
+    });
+  } else {
+    return console.log(`Error: FS operation failed`);
+  }
 };
 
-await copy();
+copy();
