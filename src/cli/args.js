74e63e9f45e9fd8b5a4a882897686fc4b355fffb
KarinaBertosh <cherrycake246@gmail.com>
2023-06-01 17:17:53 +0300

    fix: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,12 +1,10 @@
 const parseArgs = () => {
   const arguments = process.argv;
-  arguments.forEach((arg) => {
-    if (arg) {
-      console.log(arg);
-    } else {
-      console.log("No such arguments exists");
-    }
-  });
+  console.log(
+    `${arguments[2].slice(2)} is ${arguments[3]},`,
+    `${arguments[4].slice(2)} is ${arguments[5]},`,
+    `${arguments[6].slice(2)} is ${arguments[7]}`
+  );
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,11 @@
 const parseEnv = () => {
   const prefix = "RSS_";
   const variables = process.env;
+  console.log(1, process.env.RSS_bar);
+
   for (v in variables) {
     if (v.includes(prefix)) {
       console.log(v);
-    } else {
-      console.log("No such variable exists");
     }
   }
 };
