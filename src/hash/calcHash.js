dc5b5e26541b7d87a9dbdc7294e544413a49d661
KarinaBertosh <cherrycake246@gmail.com>
2023-06-02 10:50:02 +0300

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
 const calculateHash = async () => {
-    // Write your code here 
+  const crypto = require("crypto");
+  const fs = require("fs");
+  const str = "I need to be hashed 😃!";
+
+  const hashNode = (val) =>
+    new Promise((resolve) =>
+      setTimeout(
+        () => resolve(crypto.createHash("sha256").update(val).digest("hex")),
+        0
+      )
+    );
+
+  hashNode(str).then(console.log);
 };
 
-await calculateHash();
+calculateHash();
