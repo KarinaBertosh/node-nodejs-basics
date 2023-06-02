c7f110c46b5c10e88e795f88d8d9bc4981d65e8e
KarinaBertosh <cherrycake246@gmail.com>
2023-06-02 16:35:09 +0300

    fix: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,12 @@
 const read = () => {
-const fs = require("fs");
-const process = require("process");
-const file = "./src/streams/files/fileToRead.txt";
+  const fs = require("fs");
+  const process = require("process");
+  const file = "./src/streams/files/fileToRead.txt";
 
-lf = fs.createReadStream(file);
-lf.on("open", () => {
-  lf.pipe(process.stdout);
-});
+  lf = fs.createReadStream(file);
+  lf.on("open", () => {
+    lf.pipe(process.stdout);
+  });
 };
 
 read();
