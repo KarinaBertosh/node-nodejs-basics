a1efe305e6e816bcf2aade876ccfb6376f84b782
KarinaBertosh <cherrycake246@gmail.com>
2023-06-02 16:34:39 +0300

    feat: read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,7 +1,12 @@
 const read = () => {
-  const fs = require("fs");
-  const myReadStream = fs.createReadStream(__dirname + `/fileToRead.txt`);
-  process.stdout.write(myReadStream);
+const fs = require("fs");
+const process = require("process");
+const file = "./src/streams/files/fileToRead.txt";
+
+lf = fs.createReadStream(file);
+lf.on("open", () => {
+  lf.pipe(process.stdout);
+});
 };
 
 read();
