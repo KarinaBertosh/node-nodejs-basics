827a0e44f6e78ad48a0dfa3cad23014ee5b4be04
KarinaBertosh <cherrycake246@gmail.com>
2023-06-02 17:03:29 +0300

    feat: write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
-const write = async () => {
-    // Write your code here 
+const write = () => {
+  const fs = require("fs");
+  var stream = require("stream");
+  var util = require("util");
+  const file = "./src/streams/files/fileToWrite.txt";
+
+  function EchoStream() {
+    stream.Writable.call(this);
+  }
+  
+  util.inherits(EchoStream, stream.Writable);
+  EchoStream.prototype._write = function (chunk, encoding, done) {
+    fs.createWriteStream(file).write(chunk.toString());
+    done();
+  };
+
+  var myStream = new EchoStream();
+  process.stdin.pipe(myStream);
 };
 
-await write();
+write();
