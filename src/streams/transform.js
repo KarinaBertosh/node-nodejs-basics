d3d62bcb319564efbd6e23ccdc264706cb9568d2
KarinaBertosh <cherrycake246@gmail.com>
2023-06-03 13:09:21 +0300

    feat: transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
-const transform = async () => {
-    // Write your code here 
+const transform = () => {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  rl.on("line", (line) => {
+    const result = line.split("").reverse().join("");
+    console.log(result);
+  });
+  rl.once("close", () => {});
 };
 
-await transform();
+transform();
