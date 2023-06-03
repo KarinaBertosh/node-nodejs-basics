5fcf2dee004bce41f39e0e266eecaaff3caeaac2
KarinaBertosh <cherrycake246@gmail.com>
2023-06-03 14:29:26 +0300

    feat: compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
-const compress = async () => {
-    // Write your code here 
+const compress = () => {
+  const { createReadStream, createWriteStream } = require("fs");
+  const { createGzip } = require("zlib");
+
+  const compressFile = (path) => {
+    const handleStream = createReadStream(path);
+    handleStream
+      .pipe(createGzip())
+      .pipe(createWriteStream(`archive.gz`))
+      .on("finish", () => {
+        console.log("Compression completed successfully");
+      });
+  };
+  compressFile("./src/zip/files/fileToCompress.txt");
 };
 
-await compress();
+compress();
