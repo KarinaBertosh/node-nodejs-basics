15ddc545bda7a2679d727e7d9d27536a4e110372
KarinaBertosh <cherrycake246@gmail.com>
2023-06-08 16:10:25 +0300

    feat: add worker in main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
-const performCalculations = async () => {
-    // Write your code here
+const performCalculations = () => {
+  const os = require("os");
+  const cpuData = os.cpus().length;
+  const { Worker } = require("worker_threads");
+  console.log("Start Program");
+
+  const runService = () => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker("./src/wt/main.js", {});
+      worker.on("message", resolve);
+      worker.on("error", reject);
+      worker.on("exit", (code) => {
+        if (code != 0) {
+          reject(new Error("Worker has stopped"));
+        }
+      });
+    });
+  };
+
+  const run = async () => {
+    const result = await runService();
+    console.log(result);
+  };
+
+  for (let i = 0; i < cpuData; i++) {
+    run().catch((error) => console.log(error));
+  }
+
+  setTimeout(() => console.log("End Program"), 2000);
 };
 
-await performCalculations();
+performCalculations();
