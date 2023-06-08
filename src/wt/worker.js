cc6a76f9214193687e2fd3e328fa5f8771f0ce63
KarinaBertosh <cherrycake246@gmail.com>
2023-06-08 12:50:26 +0300

    feat: worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const { isMainThread } = require("worker_threads");
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (isMainThread) {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+    const nthFibonacci = (n) =>
+      n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+    const result = nthFibonacci(getRandomInt(10));
+    console.log(result);
+    return result;
+  } else {
+    new Worker("./src/wt/worker.js");
+    console.log("Inside Worker!");
+    console.log(isMainThread);
+  }
 };
 
 sendResult();
