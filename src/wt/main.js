50629f4f3e1d2e28936058b3acb581e06f41b684
KarinaBertosh <cherrycake246@gmail.com>
2023-06-09 15:33:11 +0300

    fix: main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,34 +1,45 @@
 const performCalculations = () => {
+  const { Worker } = require("worker_threads");
   const os = require("os");
   const cpuData = os.cpus().length;
-  const { Worker } = require("worker_threads");
+  const arr = [];
 
-  const runService = () => {
+  const getValue = (i) => {
+    const result = 10 + i;
+    return result;
+  };
+
+  const runService = (i) => {
     return new Promise((resolve, reject) => {
       const worker = new Worker("./src/wt/worker.js", {
         workerData: {
-          value: 14,
+          value: getValue(i),
         },
       });
       worker.on("message", (result) => {
-        console.log(result);
+        arr.push(result);
       });
       worker.on("error", reject);
       worker.on("exit", (code) => {
         if (code != 0) {
           reject(new Error("Worker has stopped"));
         }
+        console.log(arr);
       });
     });
   };
 
-  const run = async () => {
-    const result = await runService();
+  const run = async (i) => {
+    const result = await runService(i);
   };
 
   for (let i = 0; i < cpuData; i++) {
-    run().catch((error) => console.log(error));
+    run(i).catch((error) => console.log(error));
   }
+
+  // console.log(arr);
+
+  // setTimeout(() => console.log(arr), 2000);
 };
 
 performCalculations();
