62475217654df00ba987bd5737a8e39410e461c5
KarinaBertosh <cherrycake246@gmail.com>
2023-06-19 22:17:34 +0300

    fix: write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,22 +1,8 @@
 const fs = require("fs");
-var stream = require("stream");
-var util = require("util");
 
 const write = () => {
-  const file = "./src/streams/files/fileToWrite.txt";
-
-  function EchoStream() {
-    stream.Writable.call(this);
-  }
-  
-  util.inherits(EchoStream, stream.Writable);
-  EchoStream.prototype._write = function (chunk, encoding, done) {
-    fs.createWriteStream(file).write(chunk.toString());
-    done();
-  };
-
-  var myStream = new EchoStream();
-  process.stdin.pipe(myStream);
+  const file = fs.createWriteStream("./src/streams/files/fileToWrite.txt");
+  process.stdin.pipe(file);
 };
 
 write();
